451ade7ff4a5ef4ed3c8cc9f50cfef65b1fafc58
Pashinuyk <149459630+Pashinuyk@users.noreply.github.com>
2025-10-24 13:31:16 +1000

    Add files via upload

--- TEST/code.js
+++ TEST/code.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function() {
     //ПРОСТО ВСТАВКА
     mas = ["LYUDI", "VY", "HUY NA BLYUDE", "PUTIN"]
 
+    d3.selectAll("ul").selectAll("li").attr('class','red')
+
     //d3.select("ul").selectAll("li").data(mas).enter().insert("li", "li.HUI").attr("class", "PUTIN")
     //d3.select("ul").selectAll("li").data(mas).exit().text("ПОБЕДА")
     //d3.selectAll("li").data(mas).text(d => d)
